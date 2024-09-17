c406bb864d6e88e97096ce934825018156e5eced
computerkingminsu <ya5656@naver.com>
2024-09-18 01:58:58 +0900

    feat: selectclass style change, add filter student problems

--- src/app/student/[class]/problems/page.tsx
+++ src/app/student/[class]/problems/page.tsx
@@ -1,12 +1,53 @@
 'use client';
 
+import { Select } from 'antd';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
 import { IoSearchSharp } from 'react-icons/io5';
 
+const { Option } = Select;
+
 export default function Problems() {
-  const list = Array.from({ length: 60 }, (_, i) => i + 1);
+  const [selectedSolved, setSelectedSolved] = useState<string | null>(null);
+  const [selectedLevel, setSelectedLevel] = useState<string | null>(null);
+  const [selectedSubmission, setSelectedSubmission] = useState<string | null>(
+    null,
+  );
+  const [selectedAccuracy, setSelectedAccuracy] = useState<string | null>(null);
+
+  const handleSolvedChange = (value: string | null) => {
+    setSelectedSolved(value);
+  };
+
+  const handleLevelChange = (value: string | null) => {
+    setSelectedLevel(value);
+  };
+
+  const handleSubmissionChange = (value: string | null) => {
+    setSelectedSubmission(value);
+  };
+
+  const handleAccuracyChange = (value: string | null) => {
+    setSelectedAccuracy(value);
+  };
+
+  const list = Array.from({ length: 60 }, (_, i) => {
+    const randomLevel = Math.floor(Math.random() * 3) + 1;
+    const randomSolved = Math.random() > 0.5 ? 'solved' : 'unsolved';
+    const randomSubmission = Math.floor(Math.random() * 100) + 1;
+    const randomAccuracy = Math.floor(Math.random() * 101);
+
+    return {
+      id: i + 1,
+      name: `피라미드 별찍기${i + 1}`,
+      level: `Lv.${randomLevel}`,
+      solved: randomSolved,
+      submissionCount: randomSubmission,
+      accuracyRate: randomAccuracy,
+    };
+  });
+
   const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 상태
   const itemsPerPage = 9; // 한 페이지당 항목 수
   const pagesPerBlock = 5; // 한 페이지 블록당 페이지 수
@@ -18,14 +59,36 @@ export default function Problems() {
       parts[index - 1] === 'student' && parts[index + 1] === 'problems',
   );
 
+  let filteredList = list
+    .filter((item) => (selectedSolved ? item.solved === selectedSolved : true))
+    .filter((item) =>
+      selectedLevel ? item.level === `Lv.${selectedLevel}` : true,
+    );
+
+  if (selectedSubmission) {
+    filteredList = filteredList.sort((a, b) =>
+      selectedSubmission === 'ascending'
+        ? a.submissionCount - b.submissionCount
+        : b.submissionCount - a.submissionCount,
+    );
+  }
+
+  if (selectedAccuracy) {
+    filteredList = filteredList.sort((a, b) =>
+      selectedAccuracy === 'ascending'
+        ? a.accuracyRate - b.accuracyRate
+        : b.accuracyRate - a.accuracyRate,
+    );
+  }
+
   // 현재 페이지에 해당하는 항목들 가져오기
-  const currentItems = list.slice(
+  const currentItems = filteredList.slice(
     (currentPage - 1) * itemsPerPage,
     currentPage * itemsPerPage,
   );
 
   // 총 페이지 수 계산
-  const totalPages = Math.ceil(list.length / itemsPerPage);
+  const totalPages = Math.ceil(filteredList.length / itemsPerPage);
 
   // 현재 페이지 블록에 해당하는 페이지 번호들 가져오기
   const currentBlock = Math.ceil(currentPage / pagesPerBlock);
@@ -35,39 +98,104 @@ export default function Problems() {
     { length: endPage - startPage + 1 },
     (_, i) => startPage + i,
   );
+
   return (
     <div className="w-full flex justify-center text-secondary">
       <div className="w-[70%] flex gap-12 pt-14">
         {/* left */}
         <main className="w-[75%]">
           {/* 문제 검색 */}
-          <div className="flex items-center bg-white  shadow-md rounded-md px-4">
+          <div className="flex items-center bg-white shadow-md rounded-md px-4">
             <IoSearchSharp className="text-gray-400 text-lg" />
             <input
               type="text"
-              className="w-full py-3 pl-3  focus:outline-none placeholder:text-sm text-sm"
+              className="w-full py-3 pl-3 focus:outline-none placeholder:text-sm text-sm"
               placeholder="문제 제목을 입력하세요."
             />
           </div>
-
+          <section className="flex w-[100%] gap-4 mt-3">
+            <Select
+              id="solved-select"
+              placeholder="상태를 선택하세요."
+              value={selectedSolved}
+              onChange={handleSolvedChange}
+              className="w-full shadow-md custom-select"
+              allowClear // 'x' 버튼을 통해 초기화 가능
+            >
+              <Option value="unsolved">안 푼 문제</Option>
+              <Option value="solved">푼 문제</Option>
+            </Select>
+            <Select
+              id="level-select"
+              placeholder="난이도를 선택하세요."
+              value={selectedLevel}
+              onChange={handleLevelChange}
+              className="w-full shadow-md custom-select "
+              allowClear // 'x' 버튼을 통해 초기화 가능
+            >
+              <Option value="1">Lv.1</Option>
+              <Option value="2">Lv.2</Option>
+              <Option value="3">Lv.3</Option>
+            </Select>
+            <Select
+              id="submission-select"
+              placeholder="제출 인원"
+              value={selectedSubmission}
+              onChange={handleSubmissionChange}
+              className="w-full shadow-md custom-select"
+              allowClear
+            >
+              <Option value="ascending">제출 인원 오름차순</Option>
+              <Option value="descending">제출 인원 내림차순</Option>
+            </Select>
+            <Select
+              id="accuracy-select"
+              placeholder="정답률"
+              value={selectedAccuracy}
+              onChange={handleAccuracyChange}
+              className="w-full shadow-md custom-select"
+              allowClear
+            >
+              <Option value="ascending">정답률 오름차순</Option>
+              <Option value="descending">정답률 내림차순</Option>
+            </Select>
+          </section>
           {/* 문제 목록 */}
-          <div className="mt-8 rounded-md border bg-white   shadow-md text-sm text-gray-500">
-            <div className="flex justify-between items-center py-2 px-5 border-b bg-[#eeeff3] text-gray-800 ">
-              <span className="w-[10%] ">상태</span>
-              <span className="w-[60%] ">문제 이름</span>
-              <span className="w-[20%] ">난이도</span>
+          <div className="mt-5 rounded-md border bg-white shadow-md text-sm text-gray-500">
+            <div className="flex justify-between items-center py-2 px-5 border-b bg-[#eeeff3] text-gray-800">
+              <span className="w-[10%]">상태</span>
+              <span className="w-[50%]">문제 이름</span>
+              <span className="w-[10%]">난이도</span>
               <span className="w-[10%]">제출</span>
+              <span className="w-[10%]">정답률</span>
             </div>
             {currentItems.map((item) => (
               <Link
-                href={`/student/${selectedClass}/problems/${item}`}
-                key={item}
+                href={`/student/${selectedClass}/problems/${item.id}`}
+                key={item.id}
               >
-                <div className="flex justify-between items-center text-sm py-5 px-5 border-b  hover:bg-[#eeeff3] cursor-pointer  last:border-none">
-                  <span className="w-[10%] ">✔</span>
-                  <span className="w-[60%] ">피라미드 별찍기{item}</span>
-                  <span className="w-[20%] text-primary ">Lv.1</span>
-                  <span className="w-[10%] flex items-center">{item}명</span>
+                <div className="flex justify-between items-center text-sm py-5 px-5 border-b hover:bg-[#eeeff3] cursor-pointer last:border-none">
+                  <span className="w-[10%] text-green-500 font-bold">
+                    {item.solved === 'solved' ? '✔' : ''}
+                  </span>
+                  <span className="w-[50%]">{item.name}</span>
+                  <span
+                    className={`w-[10%] font-semibold ${
+                      item.level === 'Lv.1'
+                        ? 'text-green-400'
+                        : item.level === 'Lv.2'
+                          ? 'text-sky-400'
+                          : 'text-rose-400'
+                    }`}
+                  >
+                    {item.level}
+                  </span>
+                  <span className="w-[10%] flex items-center">
+                    {item.submissionCount}명
+                  </span>
+                  <span className="w-[10%] flex items-center">
+                    {item.accuracyRate}%
+                  </span>
                 </div>
               </Link>
             ))}
@@ -84,7 +212,7 @@ export default function Problems() {
                 setCurrentPage(previousBlockStartPage);
               }}
               disabled={currentPage === 1}
-              className="px-3 py-1 bg-white rounded shadow-md  hover:bg-[#eeeff3]"
+              className="px-3 py-1 bg-white rounded shadow-md hover:bg-[#eeeff3]"
             >
               &lt;
             </button>
@@ -94,7 +222,11 @@ export default function Problems() {
                 <button
                   key={page}
                   onClick={() => setCurrentPage(page)}
-                  className={`shadow-md px-3 py-1 rounded  ${page === currentPage ? 'bg-primary text-white hover:bg-primaryButtonHover' : 'bg-white hover:bg-[#eeeff3]'}`}
+                  className={`shadow-md px-3 py-1 rounded ${
+                    page === currentPage
+                      ? 'bg-primary text-white hover:bg-primaryButtonHover'
+                      : 'bg-white hover:bg-[#eeeff3]'
+                  }`}
                 >
                   {page}
                 </button>
@@ -114,16 +246,16 @@ export default function Problems() {
             </button>
           </div>
         </main>
-        {/* right  */}
+        {/* right */}
         <aside className="flex-1 self-start p-8 text-sm bg-white shadow-md rounded-md">
           <h1 className="font-semibold text-secondary mb-8">
             기초프로그래밍 01분반
           </h1>
-          <ul className="space-y-7 text-gray-500 ">
+          <ul className="space-y-7 text-gray-500">
             <li className="hover:text-gray-700 transition cursor-pointer">
               1. 변수
             </li>
-            <li className="hover:text-gray-700 transition  cursor-pointer">
+            <li className="hover:text-gray-700 transition cursor-pointer">
               2. 문자열
             </li>
             <li className="text-primary hover:text-primaryHover transition font-semibold cursor-pointer">
--- src/app/student/selectclass/page.tsx
+++ src/app/student/selectclass/page.tsx
@@ -1,41 +1,62 @@
 import Link from 'next/link';
+import { HiOutlineComputerDesktop } from 'react-icons/hi2';
+import { FaCodeBranch } from 'react-icons/fa';
+import { AiOutlineCodeSandbox } from 'react-icons/ai';
 
 export default function SelectClass() {
   return (
-    <div
-      className="w-full relative text-primaryFont font-semibold"
-      style={{ height: 'calc(100vh - 4rem)' }}
-    >
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[30%] min-h-[35%] h-auto bg-white shadow-2xl rounded-lg flex flex-col pt-4 pb-4  ">
-        <span className="text-lg text-center">
-          강의와 분반을 선택해 주세요.
-        </span>
-        <section className="mt-4 pl-7 font-normal">
-          <span> &#8226; 기초 프로그래밍</span>
-          <ul className="space-y-2 mt-2 mb-2 pl-3">
-            <Link href="/student/기초프로그래밍-01분반/problems">
-              <li className="cursor-pointer hover:text-secondaryHover transition">
-                - 01분반
-              </li>
-            </Link>
-          </ul>
-          <span> &#8226; 심화 프로그래밍</span>
-          <ul className="space-y-2 mt-2 mb-2 pl-3">
-            <Link href="/student/심화프로그래밍-02분반/problems">
-              <li className="cursor-pointer hover:text-secondaryHover transition">
-                - 02분반
-              </li>
-            </Link>
-          </ul>
-          <span> &#8226; 자바 프로그래밍</span>
-          <ul className="space-y-2 mt-2 pl-3">
-            <Link href="/student/자바프로그래밍-01분반/problems">
-              <li className="cursor-pointer hover:text-secondaryHover transition">
-                - 02분반
-              </li>
-            </Link>
-          </ul>
-        </section>
+    <div className="text-primaryFont font-semibold">
+      {/* Title */}
+      <h1 className="mt-24 text-2xl text-center block mb-14 text-primary">
+        과목을 선택해 주세요
+      </h1>
+
+      {/* Cards for class selection */}
+      <div className="flex justify-center space-x-16">
+        {/* Card for 기초 프로그래밍 */}
+        <Link href="/student/기초프로그래밍-01분반/problems">
+          <div className="cursor-pointer bg-white shadow-xl rounded-3xl py-20 px-14 border-[3px] border-transparent hover:border-blue-400 transition-all">
+            <div className="flex flex-col items-center">
+              <div className="bg-gradient-to-t from-blue-100 to-blue-400 rounded-full p-8 mb-4">
+                <HiOutlineComputerDesktop className="text-[6rem] text-white" />
+              </div>
+              <span className="mt-4 text-lg text-gray-800">
+                기초 프로그래밍
+              </span>
+              <span className=" mt-1 text-gray-500">01분반</span>
+            </div>
+          </div>
+        </Link>
+
+        {/* Card for 심화 프로그래밍 */}
+        <Link href="/student/심화프로그래밍-01분반/problems">
+          <div className="cursor-pointer bg-white shadow-xl rounded-3xl py-20 px-14 border-[3px] border-transparent  hover:border-blue-400 transition-all">
+            <div className="flex flex-col items-center">
+              <div className="bg-gradient-to-t from-blue-100 to-blue-400 rounded-full p-8 mb-4">
+                <FaCodeBranch className="text-[6rem] text-white" />
+              </div>
+              <span className="mt-4 text-lg text-gray-800">
+                심화 프로그래밍
+              </span>
+              <span className=" mt-1 text-gray-500">01분반</span>
+            </div>
+          </div>
+        </Link>
+
+        {/* Card for 자바 프로그래밍 */}
+        <Link href="/student/자바프로그래밍-01분반/problems">
+          <div className="cursor-pointer bg-white shadow-xl rounded-3xl py-20 px-14 border-[3px] border-transparent  hover:border-blue-400 transition-all">
+            <div className="flex flex-col items-center">
+              <div className="bg-gradient-to-t from-blue-100 to-blue-400 rounded-full p-8 mb-4">
+                <AiOutlineCodeSandbox className="text-[6rem] text-white" />
+              </div>
+              <span className="mt-4 text-lg text-gray-800">
+                자바 프로그래밍
+              </span>
+              <span className=" mt-1 text-gray-500">01분반</span>
+            </div>
+          </div>
+        </Link>
       </div>
     </div>
   );
