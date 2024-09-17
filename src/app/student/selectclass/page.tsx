f56c1e7b5c80bc18533f1b0030512141b68941bd
computerkingminsu <ya5656@naver.com>
2024-09-18 02:05:36 +0900

    feat: selectclass style change

--- src/app/student/selectclass/page.tsx
+++ src/app/student/selectclass/page.tsx
@@ -5,15 +5,11 @@ import { AiOutlineCodeSandbox } from 'react-icons/ai';
 
 export default function SelectClass() {
   return (
-    <div className="text-primaryFont font-semibold">
-      {/* Title */}
-      <h1 className="mt-24 text-2xl text-center block mb-14 text-primary">
-        과목을 선택해 주세요
-      </h1>
-
-      {/* Cards for class selection */}
-      <div className="flex justify-center space-x-16">
-        {/* Card for 기초 프로그래밍 */}
+    <div
+      className="font-semibold flex justify-center items-center"
+      style={{ height: 'calc(100vh - 4rem)' }}
+    >
+      <section className="flex justify-center space-x-16 mb-[4rem]">
         <Link href="/student/기초프로그래밍-01분반/problems">
           <div className="cursor-pointer bg-white shadow-xl rounded-3xl py-20 px-14 border-[3px] border-transparent hover:border-blue-400 transition-all">
             <div className="flex flex-col items-center">
@@ -28,7 +24,6 @@ export default function SelectClass() {
           </div>
         </Link>
 
-        {/* Card for 심화 프로그래밍 */}
         <Link href="/student/심화프로그래밍-01분반/problems">
           <div className="cursor-pointer bg-white shadow-xl rounded-3xl py-20 px-14 border-[3px] border-transparent  hover:border-blue-400 transition-all">
             <div className="flex flex-col items-center">
@@ -43,7 +38,6 @@ export default function SelectClass() {
           </div>
         </Link>
 
-        {/* Card for 자바 프로그래밍 */}
         <Link href="/student/자바프로그래밍-01분반/problems">
           <div className="cursor-pointer bg-white shadow-xl rounded-3xl py-20 px-14 border-[3px] border-transparent  hover:border-blue-400 transition-all">
             <div className="flex flex-col items-center">
@@ -57,7 +51,7 @@ export default function SelectClass() {
             </div>
           </div>
         </Link>
-      </div>
+      </section>
     </div>
   );
 }
