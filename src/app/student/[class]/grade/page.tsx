15799a83919315aaf1263fbb89fd36533dc49541
computerkingminsu <ya5656@naver.com>
2024-09-22 17:17:53 +0900

    feat: student grade

--- src/app/student/[class]/grade/page.tsx
+++ src/app/student/[class]/grade/page.tsx
@@ -1,12 +1,73 @@
-export default function Grade() {
+'use client';
+import { Doughnut, Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  ArcElement,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  ArcElement,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
+// 좌측 도넛 차트 데이터
+const donutData = {
+  labels: [
+    '알고리즘 01 분반',
+    '알고리즘 02 분반',
+    '네트워크 보안 01 분반',
+    '네트워크 보안 02 분반',
+  ],
+  datasets: [
+    {
+      label: '학생 수',
+      data: [50, 40, 30, 20],
+      backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0'],
+      hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0'],
+    },
+  ],
+};
+
+// 도넛 차트 옵션
+const donutOptions = {
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      position: 'bottom' as const,
+      labels: {
+        boxWidth: 12,
+        padding: 12,
+      },
+    },
+  },
+  layout: {
+    padding: {
+      top: 10,
+      bottom: 0,
+    },
+  },
+};
+
+export default function Questions() {
   return (
-    <div
-      className="w-full relative text-primaryFont font-semibold"
-      style={{ height: 'calc(100vh - 4rem)' }}
-    >
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[30%] min-h-[35%] h-auto bg-white shadow-2xl rounded-lg flex flex-col pt-4 pb-4  ">
-        <span className="text-lg text-center">성적</span>
+    <div className="w-full flex flex-col items-center py-14 gap-12 justify-center  ">
+      <div className="bg-white shadow-md rounded-2xl w-[70%] flex ">
+        <div className="w-[20%]">
+          <Doughnut data={donutData} options={donutOptions} />
+        </div>
       </div>
+      <div className="bg-white shadow-md rounded-2xl w-[70%] flex">a</div>
     </div>
   );
 }
