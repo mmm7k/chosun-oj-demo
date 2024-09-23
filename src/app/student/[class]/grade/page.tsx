41e97449901f2b4c9f2089e512a9fdf555603b35
computerkingminsu <ya5656@naver.com>
2024-09-24 02:14:26 +0900

    feat: radar chart

--- src/app/student/[class]/grade/page.tsx
+++ src/app/student/[class]/grade/page.tsx
@@ -1,28 +1,27 @@
 'use client';
-import { Doughnut } from 'react-chartjs-2';
+import { Doughnut, Radar } from 'react-chartjs-2';
 import {
   Chart as ChartJS,
   ArcElement,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
   Tooltip,
-  Legend,
 } from 'chart.js';
+
+import { FaCodeBranch } from 'react-icons/fa';
+import { PiRanking } from 'react-icons/pi';
 
 ChartJS.register(
   ArcElement,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
   Tooltip,
-  // Legend,
 );
 
 export default function Questions() {
-  // 좌측 도넛 차트 데이터
+  //도넛 차트 데이터
   const donutData = {
     labels: ['Lv.1', 'Lv.2', 'Lv.3'],
     datasets: [
@@ -51,20 +50,57 @@ export default function Questions() {
   const calculatePercentage = (value: number) => {
     return ((value / total) * 100).toFixed(1); // 소수점 1자리까지 계산
   };
+
+  // 레이다 차트 데이터
+  const radarData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'My First Dataset',
+        data: [65, 59, 90, 81, 56, 55, 40],
+        fill: true,
+        backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        pointBackgroundColor: 'rgba(255,99,132,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(255,99,132,1)',
+      },
+    ],
+  };
+  // 레이다 차트 옵션
+  const radarOptions = {
+    maintainAspectRatio: false,
+    responsive: true,
+    scales: {
+      r: {
+        angleLines: {
+          display: false,
+        },
+        suggestedMin: 0,
+        suggestedMax: 100,
+        ticks: {
+          display: false, // 축의 숫자를 숨김
+        },
+      },
+    },
+  };
   return (
     <div className="w-full flex flex-col items-center py-14 gap-12 justify-center">
       <div className="bg-white shadow-md rounded-2xl w-[70%] py-10 flex-col">
-        <p className="text-lg font-semibold text-secondary pl-9 mb-7">
-          # 난이도 분포
+        <p className="text-lg font-semibold text-secondary pl-9 mb-7 flex items-center">
+          <PiRanking className="text-2xl mr-1 mt-[0.18rem] " />
+
+          <span>난이도 분포</span>
         </p>
-        <div className="flex justify-between items-center w-[90%]  mx-auto">
+        <div className="flex flex-col sm:flex-row justify-between items-center w-[90%]  mx-auto">
           {/* 도넛 차트 */}
           <div className="w-[25%] h-48">
             <Doughnut data={donutData} options={donutOptions} />
           </div>
 
           {/* 오른쪽 레이블 표시 */}
-          <div className="w-[60%] flex flex-col">
+          <div className="w-[80%] sm:w-[60%] flex flex-col">
             <div className="flex justify-between font-semibold pl-[20%] pr-[25%]">
               <span>레벨</span>
               <span>문제</span>
@@ -103,8 +139,57 @@ export default function Questions() {
           </div>
         </div>
       </div>
+      <div className="bg-white shadow-md rounded-2xl w-[70%] py-10 flex-col">
+        <p className="text-lg font-semibold text-secondary pl-9 mb-7 flex items-center">
+          <FaCodeBranch className="text-xl mr-1 mt-[0.18rem]" />
+          <span>알고리즘 분포</span>
+        </p>
+        <div className="flex flex-col sm:flex-row justify-between items-center w-[90%]  mx-auto">
+          {/* 도넛 차트 */}
+          <div className="w-[25%] h-48">
+            <Radar data={radarData} options={radarOptions} />
+          </div>
 
-      <div className="bg-white shadow-md rounded-2xl w-[70%] flex">a</div>
+          {/* 오른쪽 레이블 표시 */}
+          <div className="w-[80%] sm:w-[60%] flex flex-col">
+            <div className="flex justify-between font-semibold pl-[20%] pr-[25%]">
+              <span>레벨</span>
+              <span>문제</span>
+            </div>
+            <hr className="w-full border-b-1 border-gray-300 my-2" />
+
+            {/* Lv.1 */}
+            <div className="flex justify-between pl-[5%] pr-[25%] relative">
+              <span className="text-[#FF6384] font-semibold">Lv.1</span>
+              <span>{donutData.datasets[0].data[0]}</span>
+              <span className="absolute right-0 text-gray-400">
+                {calculatePercentage(donutData.datasets[0].data[0])}%
+              </span>
+            </div>
+            <hr className="w-full border-b-1 border-gray-300 my-2" />
+
+            {/* Lv.2 */}
+            <div className="flex justify-between pl-[5%] pr-[25%] relative">
+              <span className="text-[#36A2EB] font-semibold">Lv.2</span>
+              <span>{donutData.datasets[0].data[1]}</span>
+              <span className="absolute right-0 text-gray-400">
+                {calculatePercentage(donutData.datasets[0].data[1])}%
+              </span>
+            </div>
+            <hr className="w-full border-b-1 border-gray-300 my-2" />
+
+            {/* Lv.3 */}
+            <div className="flex justify-between pl-[5%] pr-[25%] relative">
+              <span className="text-[#FFCE56] font-semibold">Lv.3</span>
+              <span>{donutData.datasets[0].data[2]}</span>
+              <span className="absolute right-0 text-gray-400">
+                {calculatePercentage(donutData.datasets[0].data[2])}%
+              </span>
+            </div>
+            <hr className="w-full border-b-1 border-gray-300 my-2" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
