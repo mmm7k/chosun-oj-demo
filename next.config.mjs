eb61ebfe29ae2b2af5a065eebf6e81b2de96b5dc
mmm7k <ya5656@naver.com>
2024-11-14 10:54:45 +0900

    feat: ver.demo

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,11 @@ const nextConfig = {
         destination: '/admin/dashboard',
         permanent: true,
       },
+      {
+	source: '/',
+	destination: '/student',
+	permanent: true,
+      },
     ];
   },
   webpack: (config, { isServer }) => {
